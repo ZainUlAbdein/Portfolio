821b39ef999808aa08f3ae048d98e8b7bb945a6c
ZainUlAbdein <zainulabedin121zain@gmail.com>
2024-04-28 19:12:24 +0500

    updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -66,7 +66,7 @@ const Hero = () => {
           <a href="#about">
             <div
               className="w-[35px] h-[64px] rounded-3xl border-4 
-            border-french border-dim flex
+            border-french flex
             justify-center items-start p-2">
               <motion.div
                 animate={{
--- src/styles.js
+++ src/styles.js
@@ -18,9 +18,9 @@ const styles = {
   padding: 'sm:px-16 px-6 sm:py-16 py-10',
 
   heroHeadText:
-    'font-black text-eerieBlack lg:text-[80px] sm:text-[60px] xs:text-[25px] text-[25px] lg:leading-[90px] mt-2',
+    'font-black text-eerieBlack lg:text-[80px] sm:text-[60px] xs:text-[25px] text-[25px] lg:leading-[80px] mt-2',
   heroSubText:
-    'text-eerieBlack font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]',
+    'text-eerieBlack font-medium lg:text-[26px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[35px]',
 
   sectionHeadText:
     'text-eerieBlack font-black md:text-[60px] sm:text-[48px] xs:text-[40px] text-[30px] font-poppins',
